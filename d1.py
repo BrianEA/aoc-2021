0e13bb5ff7774646ac408d057e95c7179bd39390
Brian Almond <brian.almond@oneandall.com>
2021-12-01 01:38:27 -0800

    Refactored, simplified

--- d1.py
+++ d1.py
@@ -17,13 +17,13 @@ depths = [int(x) for x in open('input1.txt','r').readlines()]
 # 263""".splitlines()
 # depths = [int(x) for x in depths]
 
-# pairing up each depth reading with the next depth reading value
-totalIncreases = sum([1 for d1, d2 in zip(depths, depths[1:]) if int(d1) < int(d2)])
-print('Part 1: ', totalIncreases)
 
-# summing each depth reading with the next 2 depth reading values, at each position
+def sumIncreases(list1, list2):
+    return sum(d1 < d2 for d1, d2 in zip(list1, list2))
+
+print('Part 1: ', sumIncreases(depths, depths[1:]))
+
 window1 = [d1+d2+d3 for d1, d2, d3 in zip(depths, depths[1:], depths[2:])]
-# the same, from 1 position advanced
 window2 = [d1+d2+d3 for d1, d2, d3 in zip(depths[1:], depths[2:], depths[3:])]
-totalIncreases = sum([1 for d1, d2 in zip(window1, window2) if int(d1) < int(d2)])
-print('Part 2: ', totalIncreases)
+
+print('Part 2: ', sumIncreases(window1, window2))
